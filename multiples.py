b8baad9a690af7844f85c1643ba3d94e85b98060
dc <ihavegerms@users.noreply.github.com>
2019-05-23 10:14:58 -0500

    modified to allow any 2 numbers

--- multiples.py
+++ multiples.py
@@ -7,7 +7,26 @@ def multiples(y):
         for x in range(1000):
                 if x not in mult and x % int(y) == 0:
                         mult.append(x)
+        
+        sumMult = input('Would you like the sum of the multiples as well? [y/n] ')
+
+        if sumMult.lower() == 'y':
+            print(sum(mult))
 
         print(mult)
 
-multiples(3)
+def multiples2(x,y):
+
+        mult = []
+
+        for num in range(1000):
+                if num not in mult and num % x == 0:
+                        mult.append(num)
+                if num not in mult and num % y == 0:
+                        mult.append(num)
+        print(sum(mult))
+        print(mult)
+
+
+multiples(10)
+multiples2(3,5)
