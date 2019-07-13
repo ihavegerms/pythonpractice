c3e71d525caccf0909331f83b843e3a5c6b3c9dc
darren carpenter <ihavegerms@users.noreply.github.com>
2019-07-13 15:58:55 -0500

    database practice scripts

---
+++ database_practice/database.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import pymysql
+
+# Open database connection
+db = pymysql.connect("172.17.0.2","root","darrensmells","testdb" )
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+# execute SQL query using execute() method.
+cursor.execute("SELECT VERSION()")
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+print ("Database version : %s " % data)
+# disconnect from server
+db.close()
